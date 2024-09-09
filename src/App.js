2fc2c01f46130668536fff93a783ae8d264252d7
Chandrakant-iitbbs <chandrakantgupta681@gmail.com>
2024-09-09 19:26:52 +0530

    Made user component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import ChatLogin from './components/ChatLogin';
 import { ContactProvider } from './context/ContactProvider';
 import { ConversationsProvider } from './context/ConversationProvider';
@@ -12,8 +13,21 @@ import AskQuestion from './components/AskQuestion';
 import NavBar from './components/NavBar';
 import NavBar2 from './components/NavBar2';
 import About from './components/About';
-function App() {
+import User from './components/User';
+
+const App = () => {
+
   const [id, setId] = useLocalStorage("id", "");
+
+  const [isOnline, setIsOnline] = useState(false);;
+
+  useEffect(() => {
+    setIsOnline(true);
+    return () => {
+      setIsOnline(false);
+      console.log("User is offline");
+    };
+  }, []);
 
   const sideBar = (
     <SocketProvider id={id}>
@@ -38,12 +52,13 @@ function App() {
   return (
     <>
       <Router>
-        {/* <NavBar /> */}
-        <NavBar2 />
-        <About/>
-    
+        <NavBar />
+        {/* <NavBar2 /> */}
+
         <Routes>
+          <Route exact path="/" element={<About />} />
           <Route exact path='/login' element={<Login />} />
+          <Route exact path='/user' element={<User isOnline={isOnline} />} />
           <Route exact path="/signup" element={<SignUp />} />
           <Route exact path='/askQues' element={<AskQuestion />} />
           {/* <Route path="/" element={id ? sideBar : <ChatLogin setId={setId}/>} /> */}
